02d8aa40a8d8278351f69fba62030bbc57e73d9f
EL_Kaouther River <hk_boulaouinat@esi.dz>
2019-11-15 17:51:57 +0100

    tp_igl

---
+++ layouts/default.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-app dark>
+    
+ <v-card
+    class="ml-6 mt-10 mb-10"
+    height="700"
+    width="256"
+    fixed
+  >
+    <v-navigation-drawer
+      class="green accent-4 "
+      dark
+      permanent
+    >
+
+    <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+
+        
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+
+     <v-list>
+        
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      
+    </v-navigation-drawer>
+ </v-card>
+
+
+
+   
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    
+   
+  </v-app>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        
+        {
+          icon: 'mdi-account',
+          title: 'Profile',
+          to: '/profile'
+        },
+        {
+          icon: 'mdi-checkbox-marked-circle-outline',
+          title: 'Suivi',
+          to: '/suivi'
+        },
+         {
+          icon: 'mdi-library',
+          title: 'Cours',
+          to: '/cours'
+        },
+        {
+          icon: 'mdi-calendar-clock',
+          title: 'Emplois du temps',
+          to: '/delib'
+        },
+        {
+          icon: 'mdi-chart-line',
+          title: 'Délibiration',
+          to: '/delib'
+        },
+          {
+          icon: 'mdi-school',
+          title: 'Encadrement',
+          to: '/delib'
+        },
+         {
+          icon: 'mdi-settings',
+          title: 'Paramétre',
+          to: '/delib'
+        }
+      ],
+     
+    }
+  }
+}
+</script>
---
+++ pages/cours.vue
@@ -0,0 +1,95 @@
+<template>
+  
+    <v-flex class=" ml-10">
+      <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1 ml-10 "
+  ></v-data-table>
+    </v-flex>
+  
+</template>
+<script>
+export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Titre',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Date début', value: 'dateDebut' },
+          { text: 'Date fin', value: 'dateFin' },
+          { text: 'Description', value: 'description' },
+         
+        ],
+        desserts: [
+          {
+            name: 'cours 01',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+         {
+            name: 'cours 02',
+            dateDebut: '24 - 11 - 2019',
+            dateFin: '24 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+          {
+            name: 'cours 03',
+            dateDebut: '28 - 11 - 2019',
+            dateFin: '28 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+         
+          {
+            name: 'cours 04',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+          {
+            name: 'cours 05',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+          {
+            name: 'cours 06',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+         {
+            name: 'cours 07',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+         {
+            name: 'cours 08',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+         {
+            name: 'cours 09',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+          {
+            name: 'cours 10',
+            dateDebut: '16 - 11 - 2019',
+            dateFin: '22 - 11 - 2019',
+            description: "C'est le premier cours ",
+          },
+        ],
+      }
+    },
+  }
+</script>
